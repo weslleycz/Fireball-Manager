65b0374e39989bafa829c1bf5cc7b866e94ef12b
Weslley <weslleysouza22@gmail.com>
2022-01-03 02:33:41 -0300

    Feature: Criação do componente menu.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   extends: 'erb',
+  root: true,
+  extends: '@react-native-community',
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
--- src/main/main.ts
+++ src/main/main.ts
@@ -73,8 +73,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1200,
+    height: 638,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
