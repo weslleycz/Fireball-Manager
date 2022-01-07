25084ce68bd5cf4122dc97d6f342e17cdb63eac0
Weslley <weslleysouza22@gmail.com>
2022-01-07 03:27:11 -0300

    Componente modal e os botoes do menu junto com o sistema de fechar aplicação pelo o meu.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {
+  app,
+  Menu,
+  shell,
+  BrowserWindow,
+  MenuItemConstructorOptions,
+} from 'electron';
 import ReactDOM from 'react-dom';
 import './Menu.css';
 import Audio from './Audio';
+import ModalMensag from './Modal';
 import Logo from '../../assets/Logo.png';
+import PlayOff from '../../assets/Play_off.png';
+import PlayON from '../../assets/Play_on.png';
+import Bestiario_off from '../../assets/Bestiario_off.png';
+import Options_off from '../../assets/Options_off.png';
+import Exit_off from '../../assets/Exit_off.png';
 
 export default (props) => {
+  const [play,setValor] =useState(PlayOff);
   return (
     <div className="menu">
+      <ModalMensag
+      title="Exit"
+      text=" Tem certeza que deseja sair do Fireball Manager?"
+      buttonOne="Sim"
+      buttonTwo="Não"
+      Funcao={()=>{window.close();}}
+      />
+
       <div className="version">
         <div className="texto-protegido ">
           1.0.0
@@ -15,6 +37,13 @@ export default (props) => {
       </div>
       <div className="Logo">
         <img src={Logo} />
+      </div>
+      <div className="botoes">
+        <a href="#"><img className="meuBotao"src={play}></img></a>
+        <a href="#"><img className="meuBotao"src={Bestiario_off}></img></a>
+        <a href="#"><img className="meuBotao"src={Options_off}></img></a>
+        <a href="#"><img className="meuBotao" data-bs-toggle="modal"
+        data-bs-target="#exampleModal" src={Exit_off}></img></a>
       </div>
     </div>);
 }
